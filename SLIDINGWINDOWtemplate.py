470a7d4f994135bc9e052bfbf0c1b5e4dc2e308a
Namya LG <53875297+Namyalg@users.noreply.github.com>
2021-02-09 10:08:14 +0530

    Create SLIDINGWINDOWtemplate.py

---
+++ SLIDINGWINDOWtemplate.py
@@ -0,0 +1,18 @@
+        #start is i
+        #end is j
+        #definite window size is k
+        
+        i = 0
+        j = 0
+        k = 3
+        
+        s = 0
+        while j < len(a):
+            s += a[j]
+            if j - i + 1 < k:
+                j += 1
+            elif j - i + 1 == k:
+                print("sum is ", s)
+                s = s - a[i]
+                i += 1
+                j += 1
